33f0f9cb9e69a29252b2bf781b50c760e15d6b29
TITAN <aluochmarion7@gmail.com>
2024-05-29 14:47:22 +0300

    complete

--- lib/person.py
+++ lib/person.py
@@ -3,10 +3,13 @@
 class Person:
     # Class body goes here
     def person(self):
+        
         pass
     #Instance method definition
     def talk(self):
+
         print("Hello World!")
 
     def walk(self):
+        
         print("The person is walking.")     
